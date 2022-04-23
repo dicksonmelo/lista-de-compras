49a9f5bb6989b5a72a1771a2e0769faecbdc2bac
dickson.melo@lambda3.com.br <dicksonmelo@L3-000042.lambda3.com.br>
2022-04-23 14:29:38 -0300

    refarotando o serviço de criação para função

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express"
-import { CreateProductController } from "./controllers/CreateProductController"
+import { createProductController } from "./controllers/createProductController"
 import { DeleteProductController } from "./controllers/DeleteCategoryController"
 import { GetAllProductsController } from "./controllers/GetAllProductsController"
 import { UpdateProductController } from "./controllers/UpdateProductController"
 
 const routes = Router()
 
-routes.post("/products", new CreateProductController().handle)
+routes.post("/products", createProductController)
 routes.get("/products", new GetAllProductsController().handle)
 routes.delete("/products/:id", new DeleteProductController().handle)
 routes.put("/products/:id", new UpdateProductController().handle)
