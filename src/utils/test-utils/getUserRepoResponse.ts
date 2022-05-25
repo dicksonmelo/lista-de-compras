068c2e40c16886a59e44382e146c2d12c955bea6
dickson.melo@lambda3.com.br <dicksonmelo@L3-000042.lambda3.com.br>
2022-05-25 16:35:41 -0300

    finalizando os testes por enquanto

--- src/utils/test-utils/getUserRepoResponse.ts
+++ src/utils/test-utils/getUserRepoResponse.ts
@@ -1,24 +1,26 @@
 import AppDataSource from "../../database/dataSource"
 
 type FindOneOptions = {
-    userId: string,
-    username?: string,
-    checkIfUnencryptedPasswordIsValid?: boolean
+  userId: string
+  username?: string
+  checkIfUnencryptedPasswordIsValid?: boolean
 }
 
 export const getUserRepoResponse = (options: FindOneOptions) => {
-    const {userId, username, checkIfUnencryptedPasswordIsValid} = options
+  const { userId, username, checkIfUnencryptedPasswordIsValid } = options
 
-    const userRepo = {
-      findOne: jest.fn().mockResolvedValueOnce({
-        userId,
-        username,
-        checkIfUnencryptedPasswordIsValid: jest.fn().mockReturnValue(checkIfUnencryptedPasswordIsValid),
-      }),
-    }
-    jest
-      .mocked(AppDataSource.manager.getRepository)
-      .mockReturnValueOnce(userRepo as never)
-
-    return userRepo
+  const userRepo = {
+    findOne: jest.fn().mockResolvedValueOnce({
+      userId,
+      username,
+      checkIfUnencryptedPasswordIsValid: jest
+        .fn()
+        .mockReturnValue(checkIfUnencryptedPasswordIsValid),
+    }),
   }
+  jest
+    .mocked(AppDataSource.manager.getRepository)
+    .mockReturnValueOnce(userRepo as never)
+
+  return userRepo
+}
