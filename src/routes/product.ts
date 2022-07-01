3f434b2448834a9ad81264cf44c4aa8f549a417d
Dickson <dicksonmelo@gmail.com>
2022-07-01 16:49:06 -0300

    terminando a migração

--- src/routes/product.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { createListController } from "../controllers/product-controllers"
-import { checkJwt } from "../middlewares/checkJwt"
-
-const routes = Router()
-routes.post("/list", [checkJwt], createListController)
-
-export default routes
