d40f956c3962c4b76531791c482fb472dfac9bd3
Dickson <dicksonmelo@gmail.com>
2022-07-05 11:09:06 -0300

    corrigindo path do init

--- init.sh
+++ init.sh
@@ -1,4 +1,2 @@
-npm install
-npm run build
-cat init.sql | heroku pg:psql postgresql-clean-14556 --app app-feirinha
+cat dist/src/database/migrations/init.sql | heroku pg:psql postgresql-clean-14556 --app app-feirinha
 npm start
