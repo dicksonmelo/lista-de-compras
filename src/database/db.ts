071bc5bd9ae016b6ae766e9a018a713e752a36dc
Dickson <dicksonmelo@gmail.com>
2022-07-05 10:09:22 -0300

    ah meu amigo...

--- src/database/db.ts
+++ src/database/db.ts
@@ -6,7 +6,8 @@ dotenv.config()
 // eslint-disable-next-line max-len
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const isProduction = process.env.NODE_ENV === "production"
+// const isProduction = process.env.NODE_ENV === "production"
+const isProduction = true
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
