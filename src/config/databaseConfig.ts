8ef9e832493c387c1c2356797d1c71ebb88ec189
Dickson <dicksonmelo@gmail.com>
2022-07-05 06:29:06 -0300

    atualizando o banco

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -5,10 +5,10 @@ dotenv.config()
 const { PGUSER, PGHOST, PGPASSWORD, PGDATABASE, PGPORT } = process.env
 
 export const config = {
-  user: PGUSER as string,
-  host: PGHOST as string,
-  database: PGDATABASE as string,
-  password: PGPASSWORD as string,
+  user: PGUSER,
+  host: PGHOST,
+  database: PGDATABASE,
+  password: PGPASSWORD,
   port: Number(PGPORT),
   max: 10,
   idleTimeoutMillis: 30000,
--- src/database/db.ts
+++ src/database/db.ts
@@ -1,14 +1,25 @@
 import { Pool } from "pg"
-import { QueryResult } from "pg"
-import { config } from "../config/databaseConfig"
+import dotenv from "dotenv"
 
-const pool = new Pool(config)
+dotenv.config()
+
+// eslint-disable-next-line max-len
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+const isProduction = process.env.NODE_ENV === "production"
+
+const pool = new Pool({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+})
 
 const query = async (text: string, params = []) => {
   const start = Date.now()
   const res = await pool.query(text, params)
   const duration = Date.now() - start
-  console.log(`Duration: ${duration}\nCommand: ${res.command}\nRows: ${res.rows}\n`)
+  console.log(
+    `Duration: ${duration}\nCommand: ${res.command}\nRows: ${res.rows}\n`,
+  )
   return res
 }
 
